547cf9cde96abe9684720109aa6cdc290f0f9d8a
formallinguist <ravikiran66999@gmail.com>
2024-08-13 12:45:05 +0200

    initial commit

---
+++ scripts/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# rename gpus
+source /path/to/test_repo/scripts/rename_gpus.sh
+
+# setup basic paths
+export PROJECT_DIR=/path/to/test_repo
+export  OUTPUT_DIR=/data/users/mmosbach/logs/test_repo
+
+# cd to project dir
+cd $PROJECT_DIR
